5a8d820ab54f0ab9ace1f5f25c2fe2e712ed912c
Franchell Polanco <polanco.franchell@gmail.com>
2020-04-23 19:08:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,7 @@ def using_delete(array,string)
    array.delete(string)
 end
 
-def using_delete_at(array,2)
-  array.delete_at[2]
+def using_delete_at(array,integer)
+  array.delete_at(2)
   binding.pry
 end
